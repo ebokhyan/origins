aa72d6a6596ad6ac2f1aa4bae7382de9ecf54eec
emma-yeghoyan <emmayeghoyan@gmail.com>
2022-04-08 13:56:12 +0400

    horizontal-banner bug

--- resources/views/components/horizontal-banner-component.blade.php
+++ resources/views/components/horizontal-banner-component.blade.php
@@ -1,6 +1,8 @@
+@if(!empty($banner))
 <div class="horizontal_bnner">
     <a href="#" class="bnner_inner">
         <img src="{{asset('storage/'.$banner['image'])}}" alt="" title="" width="728" height="90"/>
         bnner title here
     </a>
 </div>
+@endif
